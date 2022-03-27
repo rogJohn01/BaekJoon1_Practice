e644c4050eb397549dc2dc874a956a53b84c0204
john1l <94162057+john1l@users.noreply.github.com>
2022-03-27 10:39:48 +0900

    [Gold IV] Title: 문자열 폭발, Time: 236 ms, Memory: 190612 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -12,9 +12,8 @@ lastChar = p[-1]
 stack = []
 for char in s:
     stack.append(char)
-    if char == lastChar:
-        if  "".join(stack[-m:]) == p:
-            del stack[-m:]
+    if char == lastChar and "".join(stack[-m:]) == p:
+        del stack[-m:]
 
 if len(stack) == 0:
     print("FRULA")
