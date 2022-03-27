c5dd01345d7e6309b7ef3caa2375bbc9d5a31943
john1l <94162057+john1l@users.noreply.github.com>
2022-03-27 14:55:26 +0900

    [Gold IV] Title: 크게 만들기, Time: 1896 ms, Memory: 33832 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -0,0 +1,19 @@
+def sol(s,k):
+    
+    st = []
+    for e in s:
+        while st and st[-1] <e and k:
+            st.pop()
+            k -=1 
+        if st or e != '0':
+            st.append(e)
+
+    if k:
+        st =st[0:-k]
+
+    return int(''.join(st))
+
+
+n, k = map(int ,input().split())
+s = input()
+print(sol(s,k))
