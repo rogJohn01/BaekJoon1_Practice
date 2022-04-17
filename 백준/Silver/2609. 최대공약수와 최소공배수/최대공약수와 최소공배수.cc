25494371b0a1628947391e190a6670b545ccfdf2
john1l <94162057+john1l@users.noreply.github.com>
2022-04-18 00:08:15 +0900

    [Silver V] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,22 +1,19 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-int gcd(int a, int b){
-    int c = a% b ; 
-    while ( c != 0) {
-            a = b; 
-            b = c; 
-            c = a % b; 
-    }
-    return b; 
+int gcd(int a, int b) {
+	if (b==0) return a ;
+
+	else return gcd(b, a%b) ;
 }
 
-int lcm(int a ,int b){
-    return ( a*b ) / gcd(a,b); 
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
 }
 
-int main() { 
-    int n1 , n2 ;
-    cin >> n1 >> n2 ;
-    cout << gcd(n1,n2) << "\n" << lcm(n1,n2) ; 
+int main() {
+	int n1, n2;
+	cin >> n1 >> n2;
+	cout << gcd(n1, n2) << "\n" << lcm(n1, n2);
 }
+
