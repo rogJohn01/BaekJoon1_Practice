151fe81ef0bc00e7e0be8dc77b92d30ccdcd3303
john1l <94162057+john1l@users.noreply.github.com>
2022-04-19 18:41:11 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/README.md
+++ 프로그래머스/lv1/12982. 예산/README.md
@@ -1,10 +1,10 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/12982) 
+[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/12982?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 4.05 MB, 시간: 0.01 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv1/12982. 예산/예산.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+  
+sort( d.begin() , d.end()) ; 
+    
+int sumv = 0; 
+for( int  i = 0 ; i < d.size() ; ++i ){
+
+	if ( sumv + d[i] <= budget ) {
+		sumv += d[i] ; }
+	else {
+		return i ;  } 
+    
+}
+    
+}
