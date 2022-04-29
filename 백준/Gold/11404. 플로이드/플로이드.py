143f86c17979a6f7e4d1a5ec05f09b48bfe87c8a
john1l <94162057+john1l@users.noreply.github.com>
2022-04-29 18:16:54 +0900

    [Gold IV] Title: 플로이드, Time: 212 ms, Memory: 116252 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,27 +1,30 @@
-from sys import stdin
-from math import inf
+import sys 
+input = sys.stdin.readline
 
-n = int(stdin.readline())
-m = int(stdin.readline())
+N = int(input())
+M = int(input())
 
-# 이동 최소비용을 저장할 2차원 배열
-cost = [[inf] * n for _ in range(n)]
-for _ in range(m):
-    a, b, c = map(int, stdin.readline().split())
-    cost[a-1][b-1] = min(cost[a-1][b-1], c)
+graph = [  [float('inf')]*(N)  for _ in range(N) ] 
 
-# 플로이드 와샬 알고리즘 적용
-for k in range(n):
-    cost[k][k] = 0
-    for i in range(n):
-        for j in range(n):
-            cost[i][j] = min(cost[i][j], cost[i][k]+cost[k][j])
+for _  in range(M):
+    x ,y , cost = map(int ,input().split() )
+    x -=1 ; y -=1 
+    graph[x][y] = min( graph[x][y] , cost)
 
-# 결과 출력
-for row in cost:
-    for i in range(n):
-        # 갈 수 없는 경로인 경우, 0 출력
-        if row[i] == inf:
-            row[i] = 0
-        print(row[i], end=" ")
-    print()
+
+for k in range(N):
+    graph[k][k] = 0 
+    for r in range(N):
+        for c in range(N):
+                graph[r][c] = min( graph[r][c] , graph[r][k] + graph[k][c]) 
+
+
+
+for r in range(N):
+    for c in range(N):
+        if graph[r][c] ==float('inf'):
+            graph[r][c] =0 
+        print( graph[r][c] , end= ' ')
+    print() 
+
+
