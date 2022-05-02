f3d6786e835de632fb346e62dc79237904b3c69e
john1l <94162057+john1l@users.noreply.github.com>
2022-05-02 20:03:26 +0900

    [Gold V] Title: 뱀, Time: 100 ms, Memory: 32508 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,55 +1,70 @@
-from collections import deque
+from collections import deque 
+
+N = int(input())
+Napple = int(input())
+appleinfo = [ list( map(int,input().split())) for _ in range(Napple) ]
+Ndir =  int(input())
+dirInfo = [ list( input().split() ) for _ in range(Ndir) ] 
+
+mat = [ [0]*N for _ in range(N) ] 
+for a, b in appleinfo:
+    mat[a-1][b-1] = 'A'
 
 
-def change(d, c):
-    # 상(0) 우(1) 하(2) 좌(3)
-    # 동쪽 회전: 상(0) -> 우(1) -> 하(2) -> 좌(3) -> 상(0) : +1 방향
-    # 왼쪽 회전: 상(0) -> 좌(3) -> 하(2) -> 우(1) -> 상(0) : -1 방향
-    if c == "L":
-        d = (d - 1) % 4
-    else:
-        d = (d + 1) % 4
-    return d
+def simul(x,y):
+
+    snake =1
+    dir = {0:[0,1] , 1:[1,0] , 2:[0,-1] , 3:[-1,0] }
+    cdir = 0
+    visit = deque()
+    time = 0
+    for t , ndir in dirInfo:
+
+        t = int(t)
+        ad = 1 if ndir=='D' else -1
+        while time !=t:
+
+            if x <0 or x>=N or y < 0 or y>=N:
+                return time
+
+            if visit and  [x,y] in visit:
+                return time
+
+            if mat[x][y] =='A':
+                snake +=1
+                mat[x][y] =0
+            elif visit:
+                visit.popleft()
 
 
-# 상 우 하 좌
-dy = [-1, 0, 1, 0]
-dx = [0, 1, 0, -1]
+            a, b = dir[cdir%4]
+            visit.append([x,y])
+            x += a ; y +=b
+            time +=1
 
+        cdir += ad
 
-def start():
-    direction = 1  # 초기 방향
-    time = 1  # 시간
-    y, x = 0, 0  # 초기 뱀 위치
-    visited = deque([[y, x]])  # 방문 위치
-    arr[y][x] = 2
     while True:
-        y, x = y + dy[direction], x + dx[direction]
-        if 0 <= y < N and 0 <= x < N and arr[y][x] != 2:
-            if not arr[y][x] == 1:  # 사과가 없는 경우
-                temp_y, temp_x = visited.popleft()
-                arr[temp_y][temp_x] = 0  # 꼬리 제거
-            arr[y][x] = 2
-            visited.append([y, x])
-            if time in times.keys():
-                direction = change(direction, times[time])
-            time += 1
-        else:  # 본인 몸에 부딪히거나, 벽에 부딪힌 경우
-            return time
+
+            if x <0 or x>=N or y < 0 or y>=N:
+                return time
+
+            if visit and  [x,y] in visit:
+                return time
+
+            if mat[x][y] =='A':
+                snake +=1
+                mat[x][y] =0
+            elif visit:
+                visit.popleft()
 
 
-if __name__ == "__main__":
+            a, b = dir[cdir%4]
+            visit.append([x,y])
+            x += a ; y +=b
+            time +=1
 
-    # input
-    N = int(input())
-    K = int(input())
-    arr = [[0] * N for _ in range(N)]
-    for _ in range(K):
-        a, b = map(int, input().split())
-        arr[a - 1][b - 1] = 1  # 사과 저장
-    L = int(input())
-    times = {}
-    for i in range(L):
-        X, C = input().split()
-        times[int(X)] = C
-    print(start())
+
+    return time
+
+print(simul(0,0)) 
