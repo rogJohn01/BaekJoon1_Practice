b05061e46b9374563cd2fe86fa9d4d5b0c2a77f8
john1l <94162057+john1l@users.noreply.github.com>
2022-05-06 12:17:43 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,20 @@
+N = int(input()) 
+mat = [ list( map(int,input().split())) for _ in range(N) ] 
+#print(mat) 
+R = N ; C =3 
+dp = [ [0]*C  for _ in range(N) ] 
+dp[0] = mat[0] 
+
+for r in range(1,R):
+    for c in range(C):
+        
+        if c==0:
+            dp[r][c] = mat[r][c] + min( dp[r-1][1] , dp[r-1][2] ) 
+        elif c==1: 
+            dp[r][c] = mat[r][c] + min( dp[r-1][0] , dp[r-1][2] ) 
+        else: # c==2:
+            dp[r][c] = mat[r][c] + min( dp[r-1][0] , dp[r-1][1] ) 
+
+#print(dp) 
+print( min( dp[R-1] )) 
+
