59d91fbb53e909a0d9b03fbcb48e14b9c373e4dd
john1l <94162057+john1l@users.noreply.github.com>
2022-05-07 11:33:18 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector> 
+#include <string> 
+#include <algorithm> 
+using namespace std; 
+
+int dp[1000001] ; 
+
+int main() { 
+
+    int N; 
+    cin >>N; 
+
+    dp[1] =1;
+    dp[2] =2; 
+    for( int i=3 ; i <=N ; i++) {
+        dp[i] = ( dp[i-1]+dp[i-2])%15746 ; }
+
+    cout << dp[N] << endl; 
+
+} 
+
