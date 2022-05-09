86cbb1f52cb273ec9a4814a6348c8640991e5b86
john1l <94162057+john1l@users.noreply.github.com>
2022-05-09 16:59:51 +0900

    [Silver IV] Title: 문서 검색, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -1,11 +1,11 @@
-import re 
-
-
-text = input() 
-word = input() 
-
-p = re.compile(f'({word})') 
-
-#print( p.findall(text) )
-print( len( p.findall(text)) ) 
-
+doc = input()
+word = input()
+count = 0
+i = 0
+while i <= len(doc) - len(word):
+    if doc[i:i + len(word)] == word:
+        count += 1
+        i += len(word) # 단어의 길이만큼 인덱스를 더해주고
+    else:              # 찾지 못하면
+        i += 1         # 1만큼 인덱스를 더해줌
+print(count)
