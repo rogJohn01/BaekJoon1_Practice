e855033dc21f59c22d50e627eb2114891fadc155
john1l <94162057+john1l@users.noreply.github.com>
2022-05-20 15:25:01 +0900

    [Gold II] Title: 공항, Time: 252 ms, Memory: 38568 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.py
+++ 백준/Gold/10775. 공항/공항.py
@@ -1,35 +1,38 @@
 import sys 
-input= sys.stdin.readline
+input = sys.stdin.readline 
+
 
 def find_parent(x):
-    if x != parent[x]:
-        parent[x] = find_parent(parent[x])
-    return parent[x]
+	if x !=parent[x]:
+		parent[x] = find_parent(parent[x])
+	return parent[x]
 
+def union_parent(x,y):
 
-def union_parent(x, y):
-    x = find_parent(x)
-    y = find_parent(y)
-    if x < y:
-        parent[y] = x
-    else:
-        parent[x] = y
+	x = find_parent(x)
+	y = find_parent(y)
+	if x < y:
+		parent[y] = x
+	else:
+		parent[x] = y 
 
+g = int(input())
+p = int(input())
 
-G = int(input())
-P = int(input())
+parent = [i for i in range(g+1)]
+plane = [] 
+for _ in range(p):
+	plane.append(int(input()))
 
-parent = [i for i in range(G+1)]
-plane = []
-for _ in range(P):
-    plane.append(int(input()))
-
-count = 0
+count = 0 
 for p in plane:
-    x = find_parent(p)
-    if x == 0:
-        break
-    union_parent(x, x-1)
-    count += 1
+	x = find_parent(p)
+	if x ==0:
+		break
+	union_parent(x,x-1)
+	count +=1 
 
 print(count)
+
+
+
