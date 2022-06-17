f788502f1b425554239e059b3b78d9ee2d5a76ca
john1l <94162057+john1l@users.noreply.github.com>
2022-06-17 12:14:33 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector> 
+#include <string> 
+#include <algorithm> 
+#include <cmath> 
+#define jl "\n"
+using namespace std; 
+#define pi 3.1415926535897932
+int main() { 
+    
+    cout << fixed;
+    cout.precision(6);
+    int r ; cin >> r ; 
+    cout << pi*pow(r,2) << jl ; 
+    cout << 2*pow(r,2) ;
+    
+} 
