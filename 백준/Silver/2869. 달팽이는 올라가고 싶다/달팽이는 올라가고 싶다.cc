8aebf5cd421d03453f43a66ff89903ec51d3a69a
john1l <94162057+john1l@users.noreply.github.com>
2022-06-20 11:07:59 +0900

    [Silver V] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -1,24 +1,9 @@
 #include <iostream>
-#include <vector> 
-#include <string> 
-#include <algorithm> 
-#include <cmath> 
-#define jl "\n"
-using namespace std; 
+using namespace std;
 
-int main() { 
-    int  A , B ,V ,days ; 
-    cin >> A >> B >> V ; 
-    
-    days =1 ;  V -= A ; 
-    if ( V % (A-B) ==0 ){ 
-        days += V / (A-B) ;
-        cout << days ; } 
-    else if( V > (A-B) ){
-        days += V / (A-B) ; 
-        cout << days +1 ; } 
-    else if( V < (A-B)) { 
-        cout << days +1 ; } 
+int main(void) {
+	int A, B, V;
+	cin >> A >> B >> V;
 
-
-} 
+	cout << (V - B -1 ) / (A - B) + 1 << endl;
+}
