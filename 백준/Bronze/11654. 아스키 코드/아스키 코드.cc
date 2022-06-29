9c361a085dcaf637c439b47bc3bdd32be91c7bac
존 <94162057+john1l@users.noreply.github.com>
2022-06-29 21:23:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector> 
+#include <string> 
+#include <algorithm> 
+#include <cmath> 
+#include <unordered_map>
+#include <map> 
+#include <queue> 
+#include <set> 
+#define jl "\n"
+using namespace std; 
+
+int main() { 
+
+    char s; 
+    cin >> s; 
+    cout << int(s) ; 
+
+
+} 
