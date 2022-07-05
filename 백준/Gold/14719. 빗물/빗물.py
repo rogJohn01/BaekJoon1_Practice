f3c7d4af1f3689c406d3f5e9429ac9dc5b66eb31
존 <94162057+john1l@users.noreply.github.com>
2022-07-05 16:37:26 +0900

    [Gold V] Title: 빗물, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -0,0 +1,21 @@
+H ,W = map(int, input().split())
+
+h =  list( map(int,input().split())) 
+
+hn = len(h)
+
+lw = [0]*hn
+rw = [0]*hn
+for l in range(1,hn):
+    lw[l] = max( lw[l-1] , h[l-1] )
+
+for r in range(hn-2,-1,-1):
+    rw[r] = max( rw[r+1] , h[r+1] )
+
+rain = 0
+for i in range(hn):
+    wlev = min( lw[i] , rw[i] )
+    if wlev > h[i]:
+        rain += wlev -h[i]
+
+print(rain) 
