df51f7463b7a6c3d9ec9aa6a368df9146cd00c60
존 <94162057+john1l@users.noreply.github.com>
2022-07-12 11:00:53 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector> 
+#include <string> 
+#include <algorithm> 
+#include <cmath> 
+#include <unordered_map>
+#include <map> 
+#include <queue> 
+#include <set> 
+#define jl "\n"
+using namespace std; 
+
+int main() { 
+    
+    string s ; cin >> s ; 
+
+    for(int a=97 ; a <123 ; a++){
+        cout << (int)s.find(char(a)) << " " ; } 
+} 
