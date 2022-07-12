5c7f0d2faa9ed6539e49481d5e00450eba5ff836
존 <94162057+john1l@users.noreply.github.com>
2022-07-12 17:42:53 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 104 ms, Memory: 32884 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.py
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.py
@@ -1,25 +1,45 @@
-import itertools
-import sys
-n=int(input())
-scv=list(map(int, input().split()))
-if n<3:
-    scv+=[0]*(3-n)
-attacks=[9, 3, 1]
-dp=[[[-1 for _ in range(61)] for _ in range(61)] for _ in range(61)]
-def hp_update(a, b, c):
-    if a<0:
-        return hp_update(0, b, c)
-    if b<0:
-        return hp_update(a, 0, c)
-    if c<0:
-        return hp_update(a, b, 0)
-    if not a and not b and not c:
-        return 0
-    if dp[a][b][c]!=-1:
-        return dp[a][b][c]
-    dp[a][b][c]=sys.maxsize
-    for damage in list(itertools.permutations(attacks)):
-        dp[a][b][c]=min(dp[a][b][c], hp_update(a-damage[0], b-damage[1], c-damage[2]))
-    dp[a][b][c]+=1
-    return dp[a][b][c]
-print(hp_update(scv[0], scv[1], scv[2]))
+from itertools import permutations
+
+import sys 
+sys.setrecursionlimit(10**7)    
+
+
+
+nums = [9,3,1] 
+casework= [] 
+for i in permutations(nums,3): 
+    casework.append(list(i))
+
+
+
+N = int(input()) 
+scvs =  list( map(int,input().split())) 
+size = max(scvs) +1 
+dp = [ [ [0]*size  for _ in range(size) ]  for _ in range(size) ] 
+inf = float('inf') 
+ans = inf 
+if len(scvs) <3:
+    scvs += [0]*(3-len(scvs)) 
+def dfs(s1,s2,s3, cnt):
+    global ans 
+  
+    if s1 <=0 and s2<= 0 and s3<= 0:  
+        ans = min(ans , cnt) 
+        return 
+    s1 = s1 if s1 >0 else 0 
+    s2 = s2 if s2 >0 else 0 
+    s3 = s3 if s3 >0 else 0 
+   
+    if( ans  < cnt): return 
+    if  dp[s1][s2][s3] and   dp[s1][s2][s3] <= cnt: return 
+
+    dp[s1][s2][s3] = cnt  
+
+    for a,b,c in casework: 
+        dfs(s1-a,s2-b ,s3-c ,cnt+1) 
+  
+dfs(scvs[0],scvs[1],scvs[2] ,0) 
+print(ans) 
+
+   
+
