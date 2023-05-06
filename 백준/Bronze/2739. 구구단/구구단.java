75719594c2942871e9395f6a5c6d176871ead0db
존 <94162057+rogJohn01@users.noreply.github.com>
2023-05-06 23:07:29 +0900

    [Bronze V] Title: 구구단, Time: 244 ms, Memory: 18600 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,19 @@
+import java.util.Scanner ; 
+
+
+
+public class Main  {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in) ; 
+
+        int N = sc.nextInt(); 
+        for(int i=1 ; i <10 ;i++){
+
+            System.out.println(Integer.toString(N)+" * "+Integer.toString(i)+" = "+Integer.toString(N*i)); 
+        }
+
+
+
+    }
+}
