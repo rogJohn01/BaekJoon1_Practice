98a2722898cce770fb19e917312bcfacf9af7034
존 <94162057+rogJohn01@users.noreply.github.com>
2023-06-06 22:56:39 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <vector> 
+#include <string> 
+#include <cstring> 
+#include <algorithm> 
+#include <cmath> 
+#include <unordered_map>
+#include <map> 
+#include <queue> 
+#include <set> 
+#define jl "\n"
+using namespace std; 
+
+double A ,B ; 
+int main() { 
+
+
+    scanf("%lf" , &A) ; 
+    scanf("%lf" , &B) ; 
+    
+    printf("%.13lf\n" , A/B ) ; 
+
+}
+
+
