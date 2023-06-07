ebca4bb98895afbfc308b3b776ad2450a1cbd273
존 <94162057+rogJohn01@users.noreply.github.com>
2023-06-07 23:46:03 +0900

    [Bronze V] Title: 시험 성적, Time: 92 ms, Memory: 12396 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val x = readLine()!!.toInt()
+
+   if(90 <=x  && x <=100 ) println("A" )
+   else if( 80<= x && x<=89) println("B")
+   else if(70 <= x && x<=79) println("C")
+   else if(60<= x && x<=69) println("D")
+   else println("F")
+
+
+
+}
