0440ffd4ed87ac484248da77bccc7456d9763169
존 <94162057+rogJohn01@users.noreply.github.com>
2023-06-08 17:04:25 +0900

    [Gold V] Title: 최소비용 구하기, Time: 568 ms, Memory: 138832 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,44 +1,33 @@
 import heapq as hq 
-
 import sys 
 input = sys.stdin.readline
 
+N = int(input()) 
+M = int(input()) 
+graph = [ [] for _ in range(N+1) ] 
+for _ in range(M):
+    a, b, cost = map(int, input().split())
+    graph[a].append([b,cost] ) 
 
-N = int(input())
-M =  int(input())
+start ,end = map(int, input().split())
+
 
 inf = float('inf') 
+dp = [inf]*(N+1) 
+dp[start] = 0 
+hp = [] 
+hq.heappush( hp , [0,start ] ) 
+while hp: 
 
-graph = [ []  for _ in range(N+1) ] 
+    w , node = hq.heappop( hp) 
+    if dp[node] < w: 
+        continue 
 
-for _ in range(M):
-    a, b, cost = map(int , input().split()) 
-    graph[a].append( [ b , cost ] )  
-
-sv , ev =  map(int , input().split()) 
-
-q = [] 
-ans = [inf] *(N+1)  
-
-def dijkstra(start):
-
-    ans[start] = 0 
-    hq.heappush(q , [ 0 , start ] ) 
-
-    while q: 
-        cw , cnode =  hq.heappop(q)
-
-        if ans[cnode]  < cw: 
-            continue 
-
-        for nnode , w in graph[cnode]: 
-
-            nw  = cw +w 
-            if nw < ans[nnode]:
-                ans[nnode] =nw 
-                hq.heappush(q , [nw ,nnode] ) 
-
-dijkstra(sv) 
-print(ans[ev]) 
+    for  nnode , nw  in graph[node]: 
+        tw = w + nw 
+        if tw < dp[nnode]: 
+            dp[nnode] = tw 
+            hq.heappush(hp , [ tw , nnode ] ) 
 
 
+print( dp[end]) 
